4017dacad8a3aaf875abd0c5b860b4f2eab51e58
Sourin <sourin.mukherjee2580@gmail.comm>
2023-10-19 13:17:28 +0530

    Added objects2.js

---
+++ objects2.js
@@ -0,0 +1,36 @@
+const JSUser = 
+{
+    gmail : "Someone@gmail.com",
+    age : 21,
+    user_details : 
+    {
+        first_name : "Sourin",
+        last_name : "Mukherjee"
+    }
+}
+
+// console.log(JSUser.user_details.first_name);
+
+
+//CONCATENATION OF OBJECTS
+const obj1 = {1: "a" , 2: "b"};
+const obj2 = {3: "c" , 4: "d"};
+const obj3 = {5: "e" , 6: "f"};
+
+
+// const obj4 = Object.assign({} , obj1 , obj2 , obj3);
+// assign sums up all object in one 
+// {} means all values are stored in a empty object then assign to obj4
+
+// const obj4 = {...obj1 , ...obj2 , ...obj3};
+// // '...' means it will spread the object then we merge it by ',' then assign to obj4
+// console.log(obj4);
+
+
+
+//accessing keys , values , entries via Objects
+console.log(Object.keys(JSUser));
+console.log("\n");
+console.log(Object.values(JSUser));
+console.log("\n");
+console.log(Object.entries(JSUser));
